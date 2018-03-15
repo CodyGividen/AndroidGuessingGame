c16a6b5fdecdb04815f2d5ca55885fbc800fecb4
CodyGividen <codygividen5@gmail.com>
2018-03-15 16:03:47 -0400

    created logic for GameActivity

--- app/src/main/java/com/example/codygividen/firstandroidworkshop/GameActivity.java
+++ app/src/main/java/com/example/codygividen/firstandroidworkshop/GameActivity.java
@@ -2,12 +2,72 @@ package com.example.codygividen.firstandroidworkshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+    private TextView clueTextView;
+    private Button guessButton;
+    private EditText guess;
+    private int randonumber;
+    private int numberOfGuess = 0;
+    private final int MAX_GUESS_COUNT = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        clueTextView = findViewById(R.id.clue_textView);
+        guessButton = findViewById(R.id.submit_guess_button);
+        guess = findViewById(R.id.guess_edittext);
+        setListener();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        randonumber = (int) Math.ceil(Math.random() * 100);
+    }
+
+    private void setListener(){
+        guessButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int userGuess = Integer.parseInt(guess.getText().toString());
+                if(userGuess > 100) {
+                    clueTextView.setText("Enter number between 1 - 100");
+                    clueTextView.setVisibility(View.VISIBLE);
+                    guess.setText("");
+                }else {
+                    checkGuess(userGuess);
+                }
+
+            }
+        });
+    }
+    private void checkGuess(int userGuess){
+        
+        if (userGuess > randonumber) {
+            clueTextView.setText("your number is to high");
+            numberOfGuess = (numberOfGuess + 1);
+            clueTextView.setVisibility(View.VISIBLE);
+            guess.setText("");
+        } else if (userGuess < randonumber) {
+            clueTextView.setText("Your number is too low!");
+            numberOfGuess = (numberOfGuess + 1);
+            clueTextView.setVisibility(View.VISIBLE);
+            guess.setText("");
+        } else if (userGuess == randonumber) {
+            //TODO -create intent to go to winning activity - handle winning
+        }else if (userGuess == MAX_GUESS_COUNT){
+            //TODO -create intent to go to winning activity - handle out of chances
+        }
+    }
+    @Override
+    public void onBackPressed() {
     }
 }
--- app/src/main/java/com/example/codygividen/firstandroidworkshop/MainActivity.java
+++ app/src/main/java/com/example/codygividen/firstandroidworkshop/MainActivity.java
@@ -3,16 +3,36 @@ package com.example.codygividen.firstandroidworkshop;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.KeyListener;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startButton;
+    private TextView directions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startGame();
+        startButton = findViewById(R.id.start_button);
+        directions = findViewById(R.id.directions_textview);
+
+        setListener();
     }
+
+    private void  setListener(){
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startGame();
+            }
+        });
+    }
+
     private void startGame(){
         Intent playGame = new Intent(this, GameActivity.class);
         startActivity(playGame);
