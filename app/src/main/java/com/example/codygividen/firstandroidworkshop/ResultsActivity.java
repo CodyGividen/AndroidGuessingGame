202925ed7ed3c9c5e89b0dda48b112cd81151796
CodyGividen <codygividen5@gmail.com>
2018-03-16 16:31:35 -0400

    finished display for results activity, and added setListener to activity.

--- app/src/main/java/com/example/codygividen/firstandroidworkshop/ResultsActivity.java
+++ app/src/main/java/com/example/codygividen/firstandroidworkshop/ResultsActivity.java
@@ -2,12 +2,39 @@ package com.example.codygividen.firstandroidworkshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
+    private Button playAgainButton;
+    private TextView correctNumberTextView;
+    private TextView resultsTextView;
+    private ImageView resultImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        playAgainButton = findViewById(R.id.restart_button);
+        correctNumberTextView = findViewById(R.id.number_textview);
+        resultsTextView = findViewById(R.id.results_textview);
+        resultImageView = findViewById(R.id. winning_imageview);
+
+        setListener();
+
+
     }
+
+    private void setListener() {
+        playAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
 }
